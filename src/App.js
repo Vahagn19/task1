e75b8189343a6b2ac736004cc102f6eff0f28403
Vahag <gevorgyanvahagn74@gmail.com>
2024-03-20 16:52:35 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
+
+
+function App() {
+  return (
+    <div className="App">
+    <DeleteSharpIcon/>
+  
+    </div>
+  );
+}
+
+export default App;
